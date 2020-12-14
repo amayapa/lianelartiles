ca0aa23e02d3abccdc18aab581b6be1010f44ae9
larts85 <larts85@yahoo.com>
2020-12-14 00:33:25 -0300

    creating structure

---
+++ src/Components/landingPage.jsx
@@ -0,0 +1,21 @@
+/* ======================= IMPORTATIONS ======================= */
+import React from 'react'
+
+const LandingPage = (props) => {
+  /* ======================= STATE ======================= */
+  
+  
+  /* ======================= FUNCTIONS ======================= */
+  
+  
+  /* ======================= RENDERING ======================= */
+  return (
+    <div className={darkMode ? 'landingDark' : 'landingLight'}>
+      <h1>Lianel Artiles</h1>
+      <h3>FullStack Web and Mobile Developer</h3>
+      <span>At last working on my pasion</span>
+    </div>
+  )
+}
+
+export default LandingPage
